9bd696b160dcc2807dcb22ce71b77b12c9927749
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-06-14 16:37:57 -0400

    Creando Boton de New Badges

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -1,11 +1,30 @@
 import React, {Component} from 'react'
 import Navbar from '../components/Navbar'
+import confLogo from '../images/badge-header.svg'
+import './styles/Badges.css'
 
 class Badges extends Component {
   render () {
     return (
       <div>
         <Navbar/>
+
+        <div className="Badges">
+          <div className="Badges__hero">
+            <div className="Badges__container">
+              <img src={confLogo} alt="Conf Logo" className="Badges_conf"/>
+            </div>
+          </div>
+        </div>
+
+        <div className="Badges__container">
+          <div className="Badges__buttons">
+            <a href="/badges/new" className="btn btn-primary">
+              New Badge
+            </a>
+          </div>
+        </div>
+
       </div>
     )
   }
