faea3e3f6ebf85674b1bd40c862ad78d300b2f73
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-06-14 14:19:27 -0400

    Implementando BadgeForm

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -3,7 +3,18 @@ import React, {Component} from 'react'
 class BadgeForm extends Component {
   render () {
     return (
-      <p>BadgeForm</p>
+      <div>
+        <h1>New Attendant</h1>
+
+        <form action="">
+          <div className="form-group">
+            <label>FirstName</label>
+            <input type="text" className="form-control" name="firstName"/>
+          </div>
+
+          <button className="btn btn-primary">Save</button>
+        </form>
+      </div>
     )
   }
 }
