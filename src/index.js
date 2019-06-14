f58fd0aeafd5ce1f7940d8cf3d7795d880fe195a
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-06-14 13:34:34 -0400

    Creando nueva pagina BadgeNew y renderizandola en el Index

--- src/index.js
+++ src/index.js
@@ -3,16 +3,8 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import './global.css';
-import Badge from './components/Badge';
+import BadgeNew from './pages/BadgeNew';
 
 const container = document.getElementById('app');
 
-ReactDOM.render(
-  <Badge
-    firstName="Richard"
-    lastName="Kaufman"
-    avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
-    jobTitle="Web Developer"
-    twitter="sparragus"
-  />
-,container);
+ReactDOM.render(<BadgeNew/>,container);
