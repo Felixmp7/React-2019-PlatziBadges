8948c2a5b2e91b4922c808eebef8004ce78b0af3
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-06-14 17:12:35 -0400

    Creando estilos css

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './styles/BadgesList.css'
 
 const BadgesList = props => {
   return (
@@ -6,10 +7,15 @@ const BadgesList = props => {
       {
         props.badges.map(badge => {
           return (
-            <li key={badge.id}>
-              <p>{badge.firstName} {badge.lastName}</p>
-              <p>{badge.twitter}</p>
-              <p>{badge.jobTitle}</p>
+            <li className="Badge_Container" key={badge.id}>
+              <div className="Badge_Avatar">
+                <img src={props.avatarUrl} alt={props.firstName}/>
+              </div>
+              <div className="Badge_Info">
+                <p className="Badge_Name">{badge.firstName} {badge.lastName}</p>
+                <p className="Badge_Twitter">@{badge.twitter}</p>
+                <p className="Badge_JobTitle">{badge.jobTitle}</p>
+              </div>
             </li>
           )
         })
