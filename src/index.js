db6a6b5cc6942be3ff152d1d289946004da751c1
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-06-14 20:27:59 -0400

    Intentando usar Fontawesome con React : FALLIDO

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { fab } from '@fortawesome/free-brands-svg-icons'
+// import { faTwitter } from '@fortawesome/free-solid-svg-icons'
 import './global.css';
 //import BadgeNew from './pages/BadgeNew';
 import Badges from './pages/Badges';
 
 const container = document.getElementById('app');
+// library.add(fab, faTwitter)
 
 ReactDOM.render(<Badges/>,container);
