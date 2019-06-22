341b320db6062224fff03e94707871854e61d904
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-06-22 18:26:31 -0400

    Separando Componente de Container

---
+++ src/pages/BadgeDetails.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import Badge from '../components/Badge'
+import confLogo from '../images/platziconf-logo.svg'
+import './styles/BadgeDetails.css'
+
+const BadgeDetails = props => {
+  return (
+    <div>
+      <div className="BadgeDetails__hero">
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <img src={confLogo} alt="Logo de la Conferencia" />
+            </div>
+            <div className="col-6 BadgeDetails__hero-attendant-name">
+              <h1>
+                {badge.firstName} {badge.lastName}
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Badge
+              firstName={badge.firstName}
+              lastName={badge.lastName}
+              twitter={badge.twitter}
+              jobTitle={badge.jobTitle}
+              email={badge.email}
+            />
+          </div>
+          <div className="col">
+            <h2>Actions</h2>
+            <React.Fragment>
+              <div>
+                <Link
+                  className="btn btn-primary mb-4"
+                  to={`/badges/${badge.id}/edit`}
+                >
+                  Edit
+                </Link>
+              </div>
+
+              <Reatc.Fragment>
+                <button className="btn btn-danger">Delete</button>
+              </Reatc.Fragment>
+            </React.Fragment>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BadgeDetails
--- src/pages/BadgeDetailsContainer.js
+++ src/pages/BadgeDetailsContainer.js
@@ -1,10 +1,7 @@
 import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
-import Badge from '../components/Badge'
+import BadgeDetails from './BadgeDetails'
 import Loader from '../components/Loader'
 import PagError from '../components/PagError'
-import confLogo from '../images/platziconf-logo.svg'
-import './styles/BadgeDetails.css'
 import API from '../API'
 
 
@@ -34,64 +31,11 @@ class BadgeDetailsContainer extends Component {
   }
 
   render () {
-    if (this.state.loading) {
-      return <Loader size={30} color={'#1B1B25'}/>
-    }
+    if (this.state.loading) { return <Loader size={30} color={'#1B1B25'}/> }
 
-    if (this.state.error) {
-      return <PagError error={this.state.error.message}/>
-    }
-    const badge = this.state.data
+    if (this.state.error) { return <PagError error={this.state.error.message}/> }
 
-    return (
-      <div>
-        <div className="BadgeDetails__hero">
-          <div className="container">
-            <div className="row">
-              <div className="col-6">
-                <img src={confLogo} alt="Logo de la Conferencia" />
-              </div>
-              <div className="col-6 BadgeDetails__hero-attendant-name">
-                <h1>
-                  {badge.firstName} {badge.lastName}
-                </h1>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <Badge
-                firstName={badge.firstName}
-                lastName={badge.lastName}
-                twitter={badge.twitter}
-                jobTitle={badge.jobTitle}
-                email={badge.email}
-              />
-            </div>
-            <div className="col">
-              <h2>Actions</h2>
-              <React.Fragment>
-                <div>
-                  <Link
-                    className="btn btn-primary mb-4"
-                    to={`/badges/${badge.id}/edit`}
-                  >
-                    Edit
-                  </Link>
-                </div>
-
-                <div>
-                  <button className="btn btn-danger">Delete</button>
-                </div>
-              </React.Fragment>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
+    return <BadgeDetails badge={this.state.data}/>
   }
 }
 
