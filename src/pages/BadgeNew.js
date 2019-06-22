eb03dabdce022b04d407acf523d5aba9586afcc5
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-06-22 18:01:02 -0400

    Renderizando Loader en BadgeNew

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -2,7 +2,7 @@ import React, {Component , Fragment} from 'react'
 import Badge from '../components/Badge'
 import BadgeForm from '../components/BadgeForm'
 import header from '../images/platziconf-logo.svg'
-import { BeatLoader } from 'react-spinners';
+import Loader from '../components/Loader'
 import API from '../API'
 import './styles/BadgeNew.css'
 
@@ -48,18 +48,7 @@ class BadgeNew extends Component {
   }
   render () {
     if (this.state.loading) {
-      return (
-        <div className="Loader__container">
-          <div className="Loader">
-            <BeatLoader
-                      sizeUnit={"px"}
-                      size={30}
-                      color={'#7DCD3F'}
-                      loading={this.state.loading}
-                    />
-          </div>
-        </div>
-      )
+      return <Loader size={30} color={'#7DCD3F'}/>
     }
     return (
       <Fragment>
